6d4fe2f409680607f34ee4445e7653f4048ac60c
Angelzt196 <75699652+Angelzt196@users.noreply.github.com>
2022-01-24 12:55:53 -0600

    Update index.js

--- public/index.js
+++ public/index.js
@@ -32,6 +32,35 @@ var students = [
         },
         career: "IDYGS"
     },
+    var students = [
+    {
+        lname: "Villicaña",
+        name: "Adolfo",
+        age: 26,
+        birthday: {
+            year: 1995,
+            month: "October",
+            day: 11,
+        },
+        Career: "IDYGS"
+    }
+]
+
+var table = document.getElementsByTagName("table")[0];
+for (var i = 0; i < students.length; i++) {
+    var templ = document.getElementById("tbodytempl").content.cloneNode(true);
+    var tdnumber = templ.querySelector("span[name='tbodynumber']");
+    var tdname = templ.querySelector("span[name='tbodyname']");
+    var tdage = templ.querySelector("span[name='tbodyage']");
+    var tdcareer = templ.querySelector("span[name='tbodycareer']");
+
+    tdnumber.innerText = i + 1;
+    tdname.innerText = students[i].lname + " " + students[i].name;
+    tdage.innerText = students[i].age;
+    tdcareer.innerText = students[i].Career;
+
+    table.append(templ);
+}
 ]
 
 var table = document.getElementsByTagName("table")[0];
